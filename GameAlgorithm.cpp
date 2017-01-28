16b79fe39e6f7500c8319c6b7e2fbfa0a552c395
Vab-jain <vabsweb@gmail.com>
2017-01-28 16:08:05 +1200

    Updating "cout<<" comments for better error handling

--- GameAlgorithm.cpp
+++ GameAlgorithm.cpp
@@ -9,19 +9,26 @@ int i=0;			//counter for path[i]
 int flag = 0;   // to check whether the path is found or not
 
 void find_path(int G[100][100], int n, int a, int b)	//n = total no. of vertices		a = robo1		b = robo2
-{	cout<<"t";
+{	
+	/*ERROR HANDLING*/
+	if(i>n)
+		{	return;	}
+	cout<<"\nfind path entering...\n";
 	if(i!=0)
 	{
 		if(path[i-1]!=a)
 		{
+			cout<<"\nparent path checked...\n";
 			path[i] = a;
 			if(a!=b)
 			{
 				for(int j=0; j<n; j++)
 				{	
+					cout<<"\nentered in loop...\n";
 					if(G[a][j]);
 					{
 						i++;
+						cout<<"\ncalling recurtion....\n";
 						find_path(G,n,j,b);
 						if(flag==0)
 							i--;
@@ -38,7 +45,7 @@ void find_path(int G[100][100], int n, int a, int b)	//n = total no. of vertices
 	{
 		path[i]=a;
 		for(int j=0; j<n; j++)
-		{	
+		{	cout<<"\nentered other else...\n";
 			if(G[a][j]);
 			{
 				i++;
@@ -77,17 +84,20 @@ int DELETE_MIN(int Path[], int PathSize, int G[100][100])
 and return the minimum time required to delete all the edges*/
 int MIN_EDGE_CUTTING_TIME(int G[100][100], int k, int RobotPosition[], int no_of_vertex)
 {
-	cout<<"t";
+	cout<<"\nEntering MIN_EDGE_CUTTING_TIME function\n";
 	int MinTime=0;  // variable to store the time taken for destroying the all edges to save kingdom
+
 	for(int l=0;l<k-1;l++)  // loops to find all the possible paths to other robot located cities from the city
-	{ 	cout<<"T";						// at ith position of the RobotPosition array in which other robot is located
+	{ 						// at ith position of the RobotPosition array in which other robot is located
 		for(int j=l+1;j<k;j++)
 		{	//int PathSize = 0;  // variable to store the size of the path array below
-			cout<<"f";
+			cout<<"\ncalling find_path in MIN_EDGE_CUTTING_TIME\n";
 			find_path(G,no_of_vertex,RobotPosition[l],RobotPosition[j]);  // getting the path of robot at i index of RobotPosition array and
-			cout<<"e";															// at j index of RobotPosition array
+			cout<<"\nsuccessfully computed find_path in MIN_EDGE_CUTTING_TIME\n";						// at j index of RobotPosition array
+			cout<<"\ncalling DELETE_MIN...\n";
 			MinTime += DELETE_MIN(path,i+1,G);   // function to delete the minimun cost edge in the path traced above
-			i=0;
+			cout<<"\nsuccessfully completed DELETE_MIN in MIN_EDGE_CUTTING_TIME\n";
+			i=0;		// reset i to 0 for path[i] in find_path
 		}
 	}	
 	return MinTime;
@@ -95,24 +105,28 @@ int MIN_EDGE_CUTTING_TIME(int G[100][100], int k, int RobotPosition[], int no_of
 
 int main()
 {
+	cout<<"\nEnter no. of vertices:\n";
 	int n, k;  // n is the no of vertices and k is the no of bots 
-	cin>>n>>k;
+	cin>>n;
+	cout<<"\nEnter no. of robots\n";
+	cin>>k;
 	int RobotPosition[k];  // to store the positions of the robots in the kingdom
 	int G[100][100]={ };  // adjacency matrix of the graph which stores the weight b/w the vertices
 	int v1, v2, w; // (v1,v2) is an edge of the graph
+	cout<<"\nEnter edges\n";
 	for(int j=0;j<n-1;j++)  // initializing the graph with weights 
 	{
 		cin>>v1>>v2>>w;  
 		G[v1][v2]=w;
 		G[v2][v1]=w;
 	}
-	cout<<"t";
+	cout<<"\nEnter robot positions:\n";
 	for(int j=0;j<k;j++)  //getting the position of the robots
-	{	cout<"t";
+	{	
 		cin>>RobotPosition[j];
 	}
 	int time;
-	cout<<"t";
+	cout<<"Callinng MIN_EDGE_CUTTING_TIME function";
 	time = MIN_EDGE_CUTTING_TIME(G,k,RobotPosition,n);
 	cout<<time;
 	//system("pause");
