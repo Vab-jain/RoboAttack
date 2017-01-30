915e6e657eec1551fa8b51ab2ef077f03b371b20
KARTHEEKCIC <bkartheekreddy@gmail.com>
2017-01-30 22:12:16 +0530

    checking the find path function with some test case

--- find_path.cpp
+++ find_path.cpp
@@ -1,64 +1,93 @@
-/*
-test graph:
-10 6
-4 6 4
-6 5 4
-6 1 2
-5 2 5
-6 7 4
-1 8 3
-6 0 2
-8 9 10
-5 3 7
-2
-4
-9
-0
-7
-5
+#include <iostream>
 
+using namespace std;
 
-		 0 1 2 3 4 5 6 7 8  9
-		_____________________
-	  0 |0 0 0 0 0 0 2 0 0  0
-	  1 |0 0 0 0 0 0 2 0 3  0
-	  2 |0 0 0 0 0 5 0 0 0  0
-	  3 |0 0 0 0 0 7 0 0 0  0
-G =   4 |0 0 0 0 0 0 4 0 0  0
-	  5 |0 0 5 7 0 0 4 0 0  0
-	  6 |2 2 0 0 4 4 0 4 0  0
-	  7 |0 0 0 0 0 0 4 0 0  0
-	  8 |0 3 0 0 0 0 0 0 0  10
-	  9 |0 0 0 0 0 0 0 0 10 0
-
-
-*/
-
-/* Method for finding path between any two robots a and b */
-
-int path[N] = {};	//path between any two robots
+int path[1000] = {};	//path between any two robots
 int i=0;			//counter for path[i]
 int flag = 0;   // to check whether the path is found or not
-
-void find_path(int G[N][N], int n, int a, int b)	//n = total no. of vertices		a = robo1		b = robo2
-{
-	if(path[i-1]!=a)
+int flag1=0;
+void find_path(int G[100][100], int n, int a, int b)	//n = total no. of vertices		a = robo1		b = robo2
+{	
+	/*ERROR HANDLING*/
+	//cout<<"\nfind path entering...\n";
+	if(i!=0)
 	{
-		path[i] = a;
-		if(a!=b)
-		{
-			for(int j=0; j<N; j++)
-			{	
-				if(G[a][j]);
-				{
-					i++;
-					find_path(G[N][N],n,j,b);
-					if(flag==0)
-						i--;
+		//if(path[i-1]!=a)
+		//{
+			//cout<<"\nparent path checked...\n";
+			path[i] = a;
+			if(a!=b)
+			{
+				for(int j=0; j<n; j++)
+				{	
+					//cout<<"\nentered in loop...\n";
+					if(G[a][j]);
+					{
+						for(int l=0;l<=i;l++)
+							if(path[l] == j)
+								flag1++;
+						if(flag1==0)
+						{	i++;
+							//cout<<"\ncalling recurtion....\n";
+							find_path(G,n,j,b);
+							if(flag==0)
+								i--;
+							else 
+								return;
+						}
+						flag1=0;
+					}
 				}
 			}
-		}
-		else 
-			flag++;
+			else 
+				flag++;
+		//}
 	}
+	else
+	{
+		path[i]=a;
+		for(int j=0; j<n; j++)
+		{	//cout<<"\nentered other else...\n";
+			if(G[a][j]);
+			{
+				i++;
+				find_path(G,n,j,b);
+				if(flag!=0)
+				//	i--;
+				//else 
+					return;
+			}
+		}	
+	}
+}
+
+int main()
+{
+	//cout<<"\nEnter no. of vertices:\n";
+	int n, k;  // n is the no of vertices and k is the no of bots 
+	cin>>n;
+	//cout<<"\nEnter no. of robots\n";
+	cin>>k;
+	int RobotPosition[k];  // to store the positions of the robots in the kingdom
+	int G[100][100]={ };  // adjacency matrix of the graph which stores the weight b/w the vertices
+	int v1, v2, w; // (v1,v2) is an edge of the graph
+	//cout<<"\nEnter edges\n";
+	for(int j=0;j<n-1;j++)  // initializing the graph with weights 
+	{
+		cin>>v1>>v2>>w;  
+		G[v1][v2]=w;
+		G[v2][v1]=w;
+	}
+	//cout<<"\nEnter robot positions:\n";
+	for(int j=0;j<k;j++)  //getting the position of the robots
+	{	
+		cin>>RobotPosition[j];
+	}
+	find_path(G,n,0,2);
+	for (int j = 0; j <= i; ++j)
+	{
+		cout<<path[j]<<" ";
+	}
+	cout<<endl;
+	return 0;
 }
